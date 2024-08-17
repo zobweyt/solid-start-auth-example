e0c3cfeeea92c4610e42b6a632fca8b0bf2917fc
zobweyt <zobweyt@gmail.com>
2024-08-17 15:23:14 +0300

    Back to client side route protection

--- src/routes/(group)/(authenticated).tsx
+++ src/routes/(group)/(authenticated).tsx
@@ -5,42 +5,43 @@ import {
   type RouteSectionProps,
 } from "@solidjs/router";
 import { Match, Switch } from "solid-js/web";
-import { $ensureIsAuthenticated, $isAuthenticated } from "~/lib/auth/server";
-
-// export const route = {
-//   preload: () => {
-//     $isAuthenticated();
-//   },
-// } satisfies RouteDefinition;
-
-// export default function Authenticated(props: RouteSectionProps) {
-//   const isAuthenticated = createAsync(() => $isAuthenticated(), {
-//     deferStream: true,
-//   });
-
-//   return (
-//     <Switch>
-//       <Match when={isAuthenticated() === true}>{props.children}</Match>
-//       <Match when={isAuthenticated() === false}>
-//         <Navigate
-//           href={`/login?redirect=${encodeURIComponent(
-//             props.location.pathname
-//           )}`}
-//         />
-//       </Match>
-//     </Switch>
-//   );
-// }
+import { $isAuthenticated } from "~/lib/auth/server";
 
 export const route = {
-  preload: ({ location }) => {
+  preload: () => {
     $isAuthenticated();
-    $ensureIsAuthenticated(
-      `/login?redirect=${encodeURIComponent(location.pathname)}`
-    );
   },
 } satisfies RouteDefinition;
 
 export default function Authenticated(props: RouteSectionProps) {
-  return props.children;
+  const isAuthenticated = createAsync(() => $isAuthenticated(), {
+    deferStream: true,
+  });
+
+  return (
+    <Switch>
+      <Match when={isAuthenticated() === true}>{props.children}</Match>
+      <Match when={isAuthenticated() === false}>
+        <Navigate
+          href={`/login?redirect=${encodeURIComponent(
+            props.location.pathname
+          )}`}
+        />
+      </Match>
+    </Switch>
+  );
 }
+
+// * This approach will temporarily display the route's contents to the user.
+// export const route = {
+//   preload: ({ location }) => {
+//     $isAuthenticated();
+//     $ensureIsAuthenticated(
+//       `/login?redirect=${encodeURIComponent(location.pathname)}`
+//     );
+//   },
+// } satisfies RouteDefinition;
+
+// export default function Authenticated(props: RouteSectionProps) {
+//   return props.children;
+// }
--- src/routes/(unauthenticated).tsx
+++ src/routes/(unauthenticated).tsx
@@ -1,37 +1,41 @@
 import {
+  createAsync,
+  Navigate,
   type RouteDefinition,
-  type RouteSectionProps
+  type RouteSectionProps,
 } from "@solidjs/router";
-import { $ensureIsUnauthenticated, $isAuthenticated } from "~/lib/auth/server";
-
-// export const route = {
-//   preload: () => {
-//     $isAuthenticated();
-//   },
-// } satisfies RouteDefinition;
-
-// export default function Unauthenticated(props: RouteSectionProps) {
-//   const isAuthenticated = createAsync(() => $isAuthenticated(), {
-//     deferStream: true,
-//   });
-
-//   return (
-//     <Switch>
-//       <Match when={isAuthenticated() === false}>{props.children}</Match>
-//       <Match when={isAuthenticated() === true}>
-//         <Navigate href="/settings" />
-//       </Match>
-//     </Switch>
-//   );
-// }
+import { Match, Switch } from "solid-js";
+import { $isAuthenticated } from "~/lib/auth/server";
 
 export const route = {
   preload: () => {
     $isAuthenticated();
-    $ensureIsUnauthenticated(`/settings`);
   },
 } satisfies RouteDefinition;
 
-export default function Authenticated(props: RouteSectionProps) {
-  return props.children;
+export default function Unauthenticated(props: RouteSectionProps) {
+  const isAuthenticated = createAsync(() => $isAuthenticated(), {
+    deferStream: true,
+  });
+
+  return (
+    <Switch>
+      <Match when={isAuthenticated() === false}>{props.children}</Match>
+      <Match when={isAuthenticated() === true}>
+        <Navigate href="/settings" />
+      </Match>
+    </Switch>
+  );
 }
+
+// * This approach will temporarily display the route's contents to the user.
+// export const route = {
+//   preload: () => {
+//     $isAuthenticated();
+//     $ensureIsUnauthenticated(`/settings`);
+//   },
+// } satisfies RouteDefinition;
+
+// export default function Authenticated(props: RouteSectionProps) {
+//   return props.children;
+// }
